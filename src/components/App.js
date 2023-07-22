334906f7a02e0b5549160ce98b0c10035c2d5e9f
TracyAntonia <aantonjuguna@gmail.com>
2023-07-22 21:05:54 +0300

    completed assignments

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,16 @@
 // create your App component here
+import React, {useState, useEffect} from "react";
+
+function App() {
+    const [dogImage, SetDogImage] = useState(null);
+    const [isLoaded, SetIsLoaded] = useState(false);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+               .then((res) => res.json())
+               .then((data) => {
+                    SetDogImage(data.message);
+                    SetIsLoaded(true);
+               })
+    })
+}
